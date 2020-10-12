0d41f333888b861ef5e2b4e9358e55db34a7b02d
DatanoiseTV <y@ext.no-route.org>
2020-10-12 09:19:48 +0000

    Initial import.

---
+++ board/raspberrypi/post-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -u
+set -e
+
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
+
+cat << EOF > ${TARGET_DIR}/etc/init.d/S99Modules
+modprobe spi_bcm2835
+modprobe snd_soc_bcm2835_i2s
+modprobe snd_soc_pisound
+modprobe snd-seq
+EOF
+
+chmod +x ${TARGET_DIR}/etc/init.d/S99Modules
